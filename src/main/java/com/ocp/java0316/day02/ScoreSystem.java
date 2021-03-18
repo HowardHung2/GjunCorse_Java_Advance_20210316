ffe8a82d590cf0b5ea754c2a6e21c2b49d5508d2
MB-study <MB-study@192.168.1.20>
2021-03-18 20:45:04 +0800

    Java-1091117-2

--- src/main/java/com/ocp/java0316/day02/ScoreSystem.java
+++ src/main/java/com/ocp/java0316/day02/ScoreSystem.java
@@ -2,6 +2,9 @@ package com.ocp.java0316.day02;
 
 public class ScoreSystem {
     public static void main(String[] args) {
-        
+        ScoreSystemMethod student1 = new ScoreSystemMethod("小明", 95, 98);
+        ScoreSystemMethod student2 = new ScoreSystemMethod("小花", 55, 78);
+        System.out.print(student1.getScoreData()); //print快捷:sout
+        System.out.print(student2.getScoreData());
     }
 }
--- src/main/java/com/ocp/java0316/day02/ScoreSystemMethod.java
+++ src/main/java/com/ocp/java0316/day02/ScoreSystemMethod.java
@@ -4,23 +4,31 @@ public class ScoreSystemMethod {
     String name;
     int chineseScore;
     int mathScore;
+
+    /*設計建構子*/
+    public ScoreSystemMethod(String name, int chineseScore, int mathScore) {
+        this.name = name;
+        this.chineseScore = chineseScore;
+        this.mathScore = mathScore;
+    }
     
     
-    
+    /*設計方法*/
     int getSum(){
-        int sum = chineseScore + mathScore;
-        return sum;
+        /*int sum = chineseScore + mathScore;
+        return sum;*/
+        return chineseScore + mathScore;
     }
     
     double getAvg(){
-        int sum = getSum();
+        /*int sum = getSum();
         double avg = sum/2;
-        return avg;
+        return avg;*/
+        return getSum()/2.0;
     }
     
     String getScoreData(){
-        String result = "姓名:" + name + " 國文分數:" + chineseScore + " 數學分數:" + mathScore ; 
-        return;
+        return String.format("姓名:%s 國文:%d 數學:%d 總分:%d 平均:%.1f", name, chineseScore, mathScore, getSum(),getAvg());
     }
     
 }
